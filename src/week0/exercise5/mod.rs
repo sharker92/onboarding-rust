5686c3896991e0e0a9b8d09ad1248a8805318646
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-05-19 16:21:10 -0600

    finished exercise5 week 0

--- src/week0/exercise5/mod.rs
+++ src/week0/exercise5/mod.rs
@@ -1,40 +1,35 @@
 pub fn network_delay_time(times: Vec<Vec<i32>>, n: i32, k: i32) -> i32 {
     let mut times2 = times.clone();
     let mut stack = Vec::new();
-    let mut time_act = 0;
-    let mut time_max = 0;
-    let mut redundant = 0;
-    stack.push(k);
+    let mut buffer = Vec::new();
+    buffer.push(vec![0, k, 0]);
+    stack.push(vec![0, 0]);
 
-    while !stack.is_empty() {
-        let last = *stack.last().unwrap();
+    while !buffer.is_empty() {
+        let mut remove = Vec::new();
+        let last = (*buffer.last().unwrap())[1];
+        if stack.iter().any(|x| x[0] == last) {
+            buffer.pop();
+            continue;
+        }
+        stack.push(vec![last, (*buffer.last().unwrap())[2]]);
+        buffer.pop();
         for (i, x) in times2.iter().enumerate() {
+
             if x[0] == last {
-                if !stack.contains(&x[1]) {
-                    stack.push(x[1]);
-                    time_act += x[2];
-                    times2.remove(i);
-                    break;
-                } else {
-                    redundant += 1;
-                }
+                remove.push(i);
             }
-
         }
-        // println!("{:?}", stack);
-        if last == *stack.last().unwrap() {
-            stack.pop();
-            if time_act > time_max {
-                time_max = time_act;
-            }
-            time_act = 0;
-            // println!("{:?}**", stack);
+        for r in remove.into_iter().rev() {
+            let mut to_push = times2.remove(r);
+            to_push[2] += (*stack.last().unwrap())[1];
+            buffer.push(to_push);
         }
-
+        buffer.sort_by_cached_key(|x| x[2]);
+        buffer.reverse();
     }
-    // println!("{}, {}", times2.len(), redundant);
-    if times2.len() > redundant {
+    if stack.len() < (n + 1) as usize {
         return -1;
     }
-    time_max
+    (*stack.last().unwrap())[1]
 }

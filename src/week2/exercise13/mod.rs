a73089e62c0a4db756c4eded8fe3e11d56f93078
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-06-09 16:10:19 -0600

    exercise 13 FINISHED! FINALLY

--- src/week2/exercise13/mod.rs
+++ src/week2/exercise13/mod.rs
@@ -1,46 +1,48 @@
 pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
-        // let s_clone = s.clone();
         let mut result = Vec::new();
-        let mut jump = 0;
+        let mut memory = false;
         if !s.is_empty() && p.len() <= s.len() {
-                for i in (0..=(s.len() - p.len())).skip(jump) {
-                        println!("-jump {}, i {}", jump, i);
+                let mut jump = 0;
+                for i in 0..=(s.len() - p.len()) {
                         if i < jump {
                                 continue;
                         }
                         let mut p_clone = p.clone();
                         let mut s_clone = s.clone();
                         let mut s_drain: String = s_clone.drain(i..(p.len() + i)).collect();
-                        println!("{} drain", s_drain);
 
-                        for s_char in s_drain.chars() {
-
-                                if let Some(p_found_index) = p_clone.find(s_char) {
-                                        println!("char: {} pfi: {}", s_char, p_found_index);
-                                        println!("{} plast", p_clone);
-                                        if s_drain.contains(&p_clone) {
-                                                println!("simon");
-                                                let last_cut = p_found_index + p_clone.len();
-                                                p_clone.drain(p_found_index..last_cut);
-
-                                        } else {
-                                                p_clone.remove(p_found_index);
-                                        }
-                                        //--s_drain.remove(0); TODO ex5
-                                        println!("{} plast", p_clone);
-                                        //println!("{}", p_clone);
-                                        if p_clone.is_empty() {
+                        while !s_drain.is_empty() {
+                                if memory {
+                                        memory = false;
+                                        let s_last = s_clone[(i - 1)..i].chars().next().unwrap();
+                                        let s_new = s_drain.pop().unwrap();
+                                        if s_new == s_last {
                                                 result.push(i as i32);
-                                                println!("vacio");
+                                                memory = true;
+                                                break;
+                                        } else {
+                                                if !p.contains(s_new) {
+                                                        jump = p.len() + i;
+                                                }
                                                 break;
                                         }
                                 } else {
-                                        if let Some(x) = s.find(s_char) {
-                                                jump = x;
+                                        memory = false;
+                                        if s_drain.contains(&p_clone) {
+                                                result.push(i as i32);
+                                                memory = true;
+                                                break;
                                         }
-                                        break;
+                                        let s_char = s_drain.remove(0);
+                                        if let Some(p_found_index) = p_clone.find(s_char) {
+                                                p_clone.remove(p_found_index);
+                                        } else {
+                                                if !p.contains(s_char) {
+                                                        jump = p.len() + i - s_drain.len();
+                                                }
+                                                break;
+                                        }
                                 }
-
                         }
                 }
         }
